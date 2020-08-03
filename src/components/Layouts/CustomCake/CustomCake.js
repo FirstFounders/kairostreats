7937f705422d58e1c261b0b554c55311b2c1cb8e
Agbamoro Victoria <45354743+vickyagba@users.noreply.github.com>
2020-08-03 17:06:23 +0100

    UI update

--- src/components/Layouts/CustomCake/CustomCake.js
+++ src/components/Layouts/CustomCake/CustomCake.js
@@ -9,35 +9,36 @@ export default function CustomCake(){
                 <h1>Customize your cake</h1>
                 <img src="https://i.ibb.co/T0bJPBn/Group-65.png" alt="Group"/>
             </div>
-
-            <div className="custom-bg">
-                {/* <div className="custom-img">
+            
+            <div className="custom-container">
+                <div className="custom-img">
                     <p>Customize a cake to your <br></br>taste and order</p>
                     <button>Customize</button>
                 
-                </div> */}
-
-                <div className="img-display">
+                </div>
+                <div className="custom-images-cake">
                     <img className="vector" src="https://i.ibb.co/42jKgyt/Group-86vector.png" alt="vector"/>
                     <div className="flex1">
-                        <div>
+                        <div className="one">
                             <img src="https://i.ibb.co/kHjh9PQ/Rectangle-19cake1.png" alt="one"/>
                         </div>
-                        <div>
+                        <div className="two">
                             <img src="https://i.ibb.co/LJKFG5X/Rectangle-20cake2.png" alt="two"/>
                         </div>
                     </div>
 
                     <div className="flex2">
-                        <div>
+                        <div className="three">
                             <img src="https://i.ibb.co/kHjh9PQ/Rectangle-19cake1.png" alt="three"/>
                         </div>
-                        <div>
-                            <img src="https://i.ibb.co/LJKFG5X/Rectangle-20cake2.png" alt="four"/>
+                        <div className="four">
+                                <img src="https://i.ibb.co/LJKFG5X/Rectangle-20cake2.png" alt="four"/>
                         </div>
                     </div>
-                </div> 
+
+                </div>
             </div>
+                
         </div>
     )
 }
--- src/components/Layouts/Specials/Specials.js
+++ src/components/Layouts/Specials/Specials.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Special.css'
-import SpecialArrowSvg from './SpecialArrowSvg'
 
 
 
@@ -12,60 +11,78 @@ function Special(){
                 <img src="https://i.ibb.co/T0bJPBn/Group-65.png" alt="Group"/>
             </div>
 
-            <div className="special-flex">
-                <div>
-                    <img src="https://i.ibb.co/1JJyMhk/Ellipse-14cake2.png" alt="Ellipse-14cake2"/>
-                    <div className="s-text">
-                        <h3>Ginger Cake</h3>
-                        <p>N 23,800</p>
+            <div className="overall-special">
+                <div className="special-flex">
+                    <div>
+                        <img src="https://i.ibb.co/1JJyMhk/Ellipse-14cake2.png" alt="Ellipse-14cake2"/>
+                        <div className="s-text">
+                            <h3>Ginger Cake</h3>
+                            <p>N 23,800</p>
+                        </div>
                     </div>
+
+                    <div>
+                        <img src="https://i.ibb.co/528TZ56/Ellipse-8cake1.png" alt="Ellipse-8cake1"/>
+                        <div className="s-text">
+                            <h3>Ginger Cake</h3>
+                            <p>N 23,800</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src="https://i.ibb.co/QXNXYHF/Ellipse-11cake4.png" alt="Ellipse-8cake1"/>
+                        <div className="s-text">
+                            <h3>Ginger Cake</h3>
+                            <p>N 23,800</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src="https://i.ibb.co/1JJyMhk/Ellipse-14cake2.png" alt="Ellipse-8cake1"/>
+                        <div className="s-text">
+                            <h3>Ginger Cake</h3>
+                            <p>N 23,800</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src="https://i.ibb.co/528TZ56/Ellipse-8cake1.png" alt="Ellipse-8cake1"/>
+                        <div className="s-text">
+                            <h3>Ginger Cake</h3>
+                            <p>N 23,800</p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <img src="https://i.ibb.co/MGSbTJf/Ellipse-13cake5.png" alt="Ellipse-8cake1"/>
+                        <div className="s-text">
+                            <h3>Ginger Cake</h3>
+                            <p>N 23,800</p>
+                        </div>
+                    </div>
+
                 </div>
 
-                <div>
-                    <img src="https://i.ibb.co/528TZ56/Ellipse-8cake1.png" alt="Ellipse-8cake1"/>
-                    <div className="s-text">
-                        <h3>Ginger Cake</h3>
-                        <p>N 23,800</p>
+                
+                <div className="special-svg">
+                    <div className="move backward">
+                    <svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.292892 7.2929C-0.0976315 7.68342 -0.0976314 8.31658 0.292893 8.70711L6.65686 15.0711C7.04738 15.4616 7.68054 15.4616 8.07107 15.0711C8.46159 14.6805 8.46159 14.0474 8.07107 13.6569L2.41421 8L8.07107 2.34315C8.46159 1.95262 8.46159 1.31946 8.07107 0.928933C7.68054 0.538409 7.04738 0.538409 6.65685 0.928933L0.292892 7.2929ZM21 7L1 7L1 9L21 9L21 7Z" fill="#000000"/>
+                    </svg>
                     </div>
-                </div>
-
-                <div>
-                    <img src="https://i.ibb.co/QXNXYHF/Ellipse-11cake4.png" alt="Ellipse-8cake1"/>
-                    <div className="s-text">
-                        <h3>Ginger Cake</h3>
-                        <p>N 23,800</p>
+                    <div className="dot"> </div>
+                    <div className="dot"> </div>
+                    <div className="dot"> </div>
+                    <div className="dot"> </div>
+                    <div className="move forward">
+                    <svg width="21" height="16" viewBox="0 0 21 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20.7071 8.70711C21.0976 8.31658 21.0976 7.68342 20.7071 7.29289L14.3431 0.928932C13.9526 0.538408 13.3195 0.538408 12.9289 0.928932C12.5384 1.31946 12.5384 1.95262 12.9289 2.34315L18.5858 8L12.9289 13.6569C12.5384 14.0474 12.5384 14.6805 12.9289 15.0711C13.3195 15.4616 13.9526 15.4616 14.3431 15.0711L20.7071 8.70711ZM0 9L20 9V7L0 7L0 9Z" fill="#000000"/>
+                    </svg>
                     </div>
+            
                 </div>
-
-                <div>
-                    <img src="https://i.ibb.co/1JJyMhk/Ellipse-14cake2.png" alt="Ellipse-8cake1"/>
-                    <div className="s-text">
-                        <h3>Ginger Cake</h3>
-                        <p>N 23,800</p>
-                    </div>
-                </div>
-
-                <div>
-                    <img src="https://i.ibb.co/528TZ56/Ellipse-8cake1.png" alt="Ellipse-8cake1"/>
-                    <div className="s-text">
-                        <h3>Ginger Cake</h3>
-                        <p>N 23,800</p>
-                    </div>
-                </div>
-
-                <div>
-                    <img src="https://i.ibb.co/MGSbTJf/Ellipse-13cake5.png" alt="Ellipse-8cake1"/>
-                    <div className="s-text">
-                        <h3>Ginger Cake</h3>
-                        <p>N 23,800</p>
-                    </div>
-                </div>
-
             </div>
 
-            <div className="special-svg">
-                <SpecialArrowSvg/>
-            </div>
 
 
         </div>
