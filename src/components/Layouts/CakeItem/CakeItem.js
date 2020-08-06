b9947a603ebbd572c2ce779f7452ea5e8488b5de
thelamina <nix4phun@gmail.com>
2020-08-06 10:57:42 +0100

    npm bug fix

--- src/components/Layouts/CakeItem/CakeItem.js
+++ src/components/Layouts/CakeItem/CakeItem.js
@@ -1,79 +1,78 @@
-import React,{useState} from 'react'
-import './CakeItem.css'
+import React, { useState } from "react";
+import "./CakeItem.css";
 
-function CakeItem(){
-    const [cart, setCart] = useState([])
+function CakeItem() {
+  const [cart, setCart] = useState([]);
 
-    const addToCart =(cake)=>{
-        console.log('Add to cart sec')
-        setCart([...cart,cake])
-    }
+  const addToCart = (cake) => {
+    console.log("Add to cart sec");
+    setCart([...cart, cake]);
+  };
 
-    const [cakes] = useState([
-        {
-            image: "https://i.ibb.co/kHjh9PQ/Rectangle-19cake1.png" ,
-            text: 'Order now',
-            name: 'Vanilla Flavour Ginger Cake',
-            category: 'Category',
-            price: '22,000'
-        },
+  const [cakes] = useState([
+    {
+      image: "https://i.ibb.co/kHjh9PQ/Rectangle-19cake1.png",
+      text: "Order now",
+      name: "Vanilla Flavour Ginger Cake",
+      category: "Category",
+      price: "22,000",
+    },
 
-        {
-            image: "https://i.ibb.co/LJKFG5X/Rectangle-20cake2.png" ,
-            text: 'Order now',
-            name: 'Vanilla Flavour Ginger Cake',
-            category: 'Category',
-            price: '22,000'
-        },
+    {
+      image: "https://i.ibb.co/LJKFG5X/Rectangle-20cake2.png",
+      text: "Order now",
+      name: "Vanilla Flavour Ginger Cake",
+      category: "Category",
+      price: "22,000",
+    },
 
-        {
-            image: "https://i.ibb.co/LJKFG5X/Rectangle-20cake2.png" ,
-            text: 'Order now',
-            name: 'Vanilla Flavour Ginger Cake',
-            category: 'Category',
-            price: '22,000'
-        },
+    {
+      image: "https://i.ibb.co/LJKFG5X/Rectangle-20cake2.png",
+      text: "Order now",
+      name: "Vanilla Flavour Ginger Cake",
+      category: "Category",
+      price: "22,000",
+    },
 
-        {
-            image:"https://i.ibb.co/kHjh9PQ/Rectangle-19cake1.png" ,
-            text: 'Order now',
-            name: 'Vanilla Flavour Ginger Cake',
-            category: 'Category',
-            price: '22,000'
-        },
-    ])
-    return(
-        <div className="c-bg">
-            <div className="c-heading">
-                <h1>Creamy Cake</h1>
-                <img src="https://i.ibb.co/T0bJPBn/Group-65.png" alt="Group"/>
+    {
+      image: "https://i.ibb.co/kHjh9PQ/Rectangle-19cake1.png",
+      text: "Order now",
+      name: "Vanilla Flavour Ginger Cake",
+      category: "Category",
+      price: "22,000",
+    },
+  ]);
+  return (
+    <div className='c-bg'>
+      <div className='c-heading'>
+        <h1>Creamy Cake</h1>
+        <img src='https://i.ibb.co/T0bJPBn/Group-65.png' alt='Group' />
+      </div>
+
+      <div className='cake-display'>
+        {cakes.map((cake, idx) => (
+          <div key={idx}>
+            <div className='cake-container'>
+              <img className='cake-gallery' src={cake.image} alt={cake.name} />
+              <div className='cake-overlay overlay-left'>
+                <div className='cake-text'>
+                  <a href='#0rdernow' onClick={() => addToCart(cake)}>
+                    {cake.text}
+                  </a>
+                </div>
+              </div>
             </div>
 
-            <div className="cake-display">
-                {cakes.map((cake, idx) =>(
-                    <div key={idx}>
-                        <div className="cake-container">
-                            <img className="cake-gallery" src={cake.image} alt={cake.name}/>
-                            <div className="cake-overlay overlay-left">
-                                <div className="cake-text">
-                                    <a href="#0rdernow" onClick={() => addToCart(cake)}>{cake.text}</a>
-                                </div>
-                            </div>
-                        </div>
-                        
-                        <div className="cake-content">
-                            <h5>{cake.name}</h5>
-                            <p>{cake.category}</p>
-                            <h4>N {cake.price}</h4>
-                        </div>
-
-                    </div>
-                ))}
+            <div className='cake-content'>
+              <h5>{cake.name}</h5>
+              <p>{cake.category}</p>
+              <h4>N {cake.price}</h4>
             </div>
-
-
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CakeItem;
