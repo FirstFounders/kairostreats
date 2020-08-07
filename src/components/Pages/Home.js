3b83e17019cc1d83146351b2a9e5161f2477392c
ugwumadu uchenna bright <36103113+josebright@users.noreply.github.com>
2020-08-07 03:11:03 +0100

    Check my changes before merging

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -3,7 +3,7 @@ import ShowCase from '../Layouts/Showcase/ShowCase'
 import CakeItem from '../Layouts/CakeItem/CakeItem'
 import CustomCake from '../Layouts/CustomCake/CustomCake'
 import Special from '../Layouts/Specials/Specials'
-import Footer from '../Layouts/Footer/Footer'
+
 
 
 function Home(){
@@ -13,7 +13,6 @@ function Home(){
             <CakeItem/>
             <CustomCake/>
             <Special/>
-            <Footer/>
         </div>
     )
 }
