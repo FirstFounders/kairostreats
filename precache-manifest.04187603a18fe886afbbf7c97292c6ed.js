cd76373bc1c802a66e088dcc2cd294f75471872e
Agbamoro Victoria <45354743+vickyagba@users.noreply.github.com>
2020-08-16 20:55:16 +0100

    Updates

---
+++ precache-manifest.04187603a18fe886afbbf7c97292c6ed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ea0770747849183e3f05b3ab5bb20c2",
+    "url": "/kairostreats/index.html"
+  },
+  {
+    "revision": "cf9733488ad32fd859c3",
+    "url": "/kairostreats/static/css/2.9c8c8187.chunk.css"
+  },
+  {
+    "revision": "2df28ae61f3e5c72e58a",
+    "url": "/kairostreats/static/css/main.33bc03b2.chunk.css"
+  },
+  {
+    "revision": "cf9733488ad32fd859c3",
+    "url": "/kairostreats/static/js/2.fb2ceafe.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kairostreats/static/js/2.fb2ceafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2df28ae61f3e5c72e58a",
+    "url": "/kairostreats/static/js/main.5641edaf.chunk.js"
+  },
+  {
+    "revision": "632b87b5c842abaae9ab",
+    "url": "/kairostreats/static/js/runtime-main.af0eb059.js"
+  },
+  {
+    "revision": "5427d75a9b7bae3ddfc016a0177fad5b",
+    "url": "/kairostreats/static/media/Group93bg.5427d75a.png"
+  }
+]);
