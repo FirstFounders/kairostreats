f06660319a31f532b2ac060f9ffb596ea614e5b7
Agbamoro Victoria <45354743+vickyagba@users.noreply.github.com>
2020-08-20 09:00:52 +0100

    page changes

--- src/components/Layouts/Showcase/ShowCase.js
+++ src/components/Layouts/Showcase/ShowCase.js
@@ -29,15 +29,15 @@ class ShowCase extends React.Component{
               slidesIndex:slides.length
           }
       }
-    //   for (i = 0; i < slides.length; i++) {
-    //     slides[i].style.display = "none";
-    //     dots[i].className = dots[i].className.replace(" active", "");
-    //   }
-    //   slides[slideIndex-1].style.display = "block";
-    //   dots[slideIndex-1].className += "active";
-    //   this.setState({
-    //       ...slidesArray
-    //   })
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className += "active";
+      this.setState({
+          ...slidesArray
+      })
     }
 
       plusSize=(n)=>{
